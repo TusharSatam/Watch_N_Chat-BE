3cc6f4f96b00defd4ffac95611e9e422822af91b
TusharSatam <tsatam91@gmail.com>
2024-03-29 22:14:37 +0530

    aded frontend url

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // server.js
 
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const { logger } = require('./src/middleware/winston.middleware');
-const { dbConnection } = require('./src/db/config');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { logger } = require("./src/middleware/winston.middleware");
+const { dbConnection } = require("./src/db/config");
 
 const app = express();
 const server = http.createServer(app);
 // Create a Socket.io instance with CORS configuration
 const io = socketIo(server, {
   cors: {
-    origin: ["http://localhost:3000"], 
+    origin: ["http://localhost:3000",  process.env.FRONTEND_URL],
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
@@ -21,16 +21,18 @@ const io = socketIo(server, {
 });
 const PORT = process.env.PORT || 5000;
 // Use cors middleware
-app.use(cors({
-    origin: 'http://localhost:3000',
-  }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", process.env.FRONTEND_URL],
+  })
+);
 // Connect to MongoDB
 dbConnection()
   .then(() => {
-    logger.info('MongoDB connected');
+    logger.info("MongoDB connected");
   })
   .catch((err) => {
-    logger.error('MongoDB connection error:', err);
+    logger.error("MongoDB connection error:", err);
   });
 
 // Define Comment schema
@@ -40,37 +42,37 @@ const commentSchema = new mongoose.Schema({
 });
 
 // Define Comment model
-const Comment = mongoose.model('Comment', commentSchema);
+const Comment = mongoose.model("Comment", commentSchema);
 
 // WebSocket handling
-io.on('connection', (socket) => {
-  logger.info('A user connected');
+io.on("connection", (socket) => {
+  logger.info("A user connected");
 
-  socket.on('createComment', async (data) => {
+  socket.on("createComment", async (data) => {
     try {
       const newComment = new Comment(data);
       await newComment.save();
-      io.emit('newComment', newComment);
+      io.emit("newComment", newComment);
     } catch (err) {
-      logger.error('Error creating comment:', err);
+      logger.error("Error creating comment:", err);
     }
   });
 
-  socket.on('disconnect', () => {
-    logger.info('A user disconnected');
+  socket.on("disconnect", () => {
+    logger.info("A user disconnected");
   });
 });
 // Route to fetch comments based on timestamp
-app.get('/comments', async (req, res) => {
-    try {
-      const { timestamp } = req.query;
-      const comments = await Comment.find({ timestamp: timestamp });
-      res.json(comments);
-    } catch (err) {
-      logger.error('Error fetching comments:', err);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
+app.get("/comments", async (req, res) => {
+  try {
+    const { timestamp } = req.query;
+    const comments = await Comment.find({ timestamp: timestamp });
+    res.json(comments);
+  } catch (err) {
+    logger.error("Error fetching comments:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
